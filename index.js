cc131e943c429fff3bdcc516a028c0314da0a733
Matthew C <m23.cullen@gmail.com>
2019-07-19 12:51:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ function deleteFromObjectByKey(object,key){
   return newRecipe;
 }
 
-function destructivelyDeleteObjectWithKeyAndValue(object,key){
+function destructivelyDeleteFromObjectByKey(object,key){
   delete object[key];
   return object;
 }
